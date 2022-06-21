7a6192d85f196faa94eaec07bbddc3117ebb0c76
Ivan Bok <bokivan@hotmail.com>
2022-06-21 22:09:04 +0800

    Dump files to S3

--- rds_getmetrics_lambda.py
+++ rds_getmetrics_lambda.py
@@ -3,13 +3,18 @@ import boto3
 import datetime
 
 cloudwatch = boto3.resource('cloudwatch')
+s3 = boto3.resource('s3')
+
 metric = cloudwatch.Metric('AWS/RDS','CPUUtilization')
 
 def lambda_handler(event, context):
+    s3_bucket_name = 'rds-cw-metrics' # Replace with your bucket name
+    rds_instance_identifier = 'mysql-test2' # Replace with your DB identifier
+    
     dimensions = [
         {
         "Name": "DBInstanceIdentifier",
-        "Value": "mysql-test2"},
+        "Value": rds_instance_identifier},
         ]
     frequency = datetime.timedelta(hours=2)
     endTime=datetime.datetime.now()
@@ -29,5 +34,16 @@ def lambda_handler(event, context):
     datapoints = response["Datapoints"]
     for datapoint in datapoints:
         datapoint["Timestamp"] = datapoint["Timestamp"].strftime("%m/%d/%Y, %H:%M:%S")
+    
+    data = json.dumps(datapoints)
+    
+    startTimeStr = startTime.strftime("%m-%d-%Y_%H-%M-%S")
+    endTimeStr = endTime.strftime("%m-%d-%Y_%H-%M-%S")
+    filename = 'rds-cpu-metrics_' + startTimeStr + '_' + endTimeStr + '.json'
+    s3object = s3.Object(s3_bucket_name,  filename)
 
-    return json.dumps(datapoints)
+    s3object.put(
+        Body=(bytes(data.encode('UTF-8')))
+    )
+    
+    return data
